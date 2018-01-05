155f3435d15396bf8a6685ff7f174bf3b8d7da25
cheezenaan <cheezenaan@gmail.com>
2018-01-05 09:58:20 +0900

    Initialize other essential gems setting

---
+++ templates/_essentials.rb
@@ -0,0 +1,28 @@
+puts "Initialize Bullet..."
+config = <<-RUBY
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.add_footer = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
+RUBY
+
+environment config, env: :development
+puts "\n"
+
+puts "Initialize BetterErrors..."
+environment 'BetterErrors::Middleware.allow_ip! "0.0.0.0/0"', env: :development
+puts "\n"
+
+puts "Initialize Spring..."
+run "bin/spring stop"
+run "bundle exec spring binstub --all"
+puts "\n"
+
+puts "Initialize Hirb..."
+copy_static_file ".pryrc"
+puts "\n"
+
--- templates/_rspec.rb
+++ templates/_rspec.rb
@@ -1,43 +1,36 @@
-def initialize_rspec
-  puts "Setting RSpec..."
-  generate "rspec:install"
-  run "rm -rf test"
+puts "Initialize RSpec..."
+generate "rspec:install"
+run "rm -rf test"
 
-  uncomment_lines "spec/rails_helper.rb", /Dir\[Rails\.root\.join/
+uncomment_lines "spec/rails_helper.rb", /Dir\[Rails\.root\.join/
 
-  copy_static_file "spec/supports/capybara.rb"
-  copy_static_file "spec/supports/database_cleaner.rb"
-  copy_static_file "spec/supports/factory_bot.rb"
-  insert_into_file "spec/rails_helper.rb", after: /require \'rspec\/rails\'\n/ do
-    <<~RUBY
-      require "supports/capybara"
-      require "supports/database_cleaner"
-      require "supports/factory_bot"
-    RUBY
-  end
-
-  insert_into_file "spec/spec_helper.rb", before: /RSpec\.configure do \|config\|/ do
-    <<~RUBY
-      require "simplecov"
-
-      SimpleCov.start do
-        add_filter "/vendor/"
-        add_filter "/spec/"
-      end
-
-    RUBY
-  end
-  puts "\n"
+copy_static_file "spec/supports/capybara.rb"
+copy_static_file "spec/supports/database_cleaner.rb"
+copy_static_file "spec/supports/factory_bot.rb"
+insert_into_file "spec/rails_helper.rb", after: /require \'rspec\/rails\'\n/ do
+  <<~RUBY
+    require "supports/capybara"
+    require "supports/database_cleaner"
+    require "supports/factory_bot"
+  RUBY
 end
 
-def initialize_guard
-  puts "Initialize guard..."
-  run "bundle exec guard init rspec"
-  gsub_file "Guardfile",
-    /guard :rspec, cmd: "bundle exec rspec" do/,
-    'guard :rspec, cmd: "bin/rspec" do'
-  puts "\n"
-end
+insert_into_file "spec/spec_helper.rb", before: /RSpec\.configure do \|config\|/ do
+  <<~RUBY
+    require "simplecov"
 
-initialize_rspec
-initialize_guard
+    SimpleCov.start do
+      add_filter "/vendor/"
+      add_filter "/spec/"
+    end
+
+  RUBY
+end
+puts "\n"
+
+puts "Initialize Guard..."
+run "bundle exec guard init rspec"
+gsub_file "Guardfile",
+  /guard :rspec, cmd: "bundle exec rspec" do/,
+  'guard :rspec, cmd: "bin/rspec" do'
+puts "\n"
